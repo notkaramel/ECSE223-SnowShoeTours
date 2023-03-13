72526e63d87413cd6a199bda85ea7dce3c841b6f
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2023-03-13 14:26:10 -0400

    Fixed getSnowShoeTour. SnowShoeTourController is complete

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourController.java
@@ -13,64 +13,67 @@ public class SnowShoeTourController {
 	
   public static TOSnowShoeTour getSnowShoeTour(int id) {
     // TODO Implement the method
-	//Can't find tour if there is no tour
+	//Can't find tour if there are no tours for this season
 	if(!ssts.hasTours()) {
 		return null;
 	}
-	//If there is no tour with that id --> index out of range 
+	//If there is no tour with that id (index out of range) --> no tour returned
 	if (id <= 0 || id > ssts.numberOfTours()-1 ) {
 		return null;
 	}
 		
-	
+	//Create a new instance of TOSnowShoeTour
 	else {
 		try {
 			Tour tour = ssts.getTour(id);
-			int tour_cost = ssts.getPriceOfGuidePerWeek()*(tour.getStartWeek()-tour.getEndWeek());
-			TOSnowShoeTour SnowShoeTour = new TOSnowShoeTour(id,tour.getStartWeek(),tour.getEndWeek(),tour.getGuide().getAccountName(),tour.getGuide().getName(),tour_cost);
+			int tour_duration = tour.getEndWeek()-tour.getStartWeek();
+			int tour_cost = ssts.getPriceOfGuidePerWeek()*(tour_duration);
 			//TOParticipant Attributes --> Create a TOParticipant object for each participant registered for the tour 
 			int p_no = tour.numberOfParticipants();
 			ArrayList <TOParticipantCost> participants = new ArrayList<TOParticipantCost>();
 			for(int p = 0; p < p_no ; p++) {
 				Participant participant = tour.getParticipant(p);
-				//Calculate the total cost for bookable items 
+				
+				//Calculate the total cost for bookable items for the participant
 				int bookableitems = 0;
+				//iterating through list of booked items 
 				for(int i = 0;i<participant.getBookedItems().size();i++) {
 					BookedItem booked_item = participant.getBookedItem(i);
 					BookableItem bookable_item = booked_item.getItem();
 					int num_item = booked_item.getQuantity();
 					int discount = 0; 
 					int single_cost = 0;
+					
 					//Check if the item is a standalone gear or in a combo 
 					if (bookable_item instanceof Gear) {
 						Gear gear = (Gear)bookable_item;
-						single_cost = gear.getPricePerWeek() * ssts.getNrWeeks();
+						single_cost = gear.getPricePerWeek() * tour_duration;
 						
 					}else if(bookable_item instanceof Combo) {
 						Combo combo = (Combo) bookable_item;
 						discount = combo.getDiscount();
+						
 						//add the price of each type of item in the combo
+						//Iterating through each type of gear
 						for(int ci = 0; ci<combo.getBookedItems().size();ci ++) {
 							ComboItem combo_item= combo.getComboItem(ci);
 							int num_ci = combo_item.getQuantity();
-							single_cost = single_cost + combo_item.getGear().getPricePerWeek() * ssts.getNrWeeks()*num_ci;
-
+							single_cost = single_cost + combo_item.getGear().getPricePerWeek()*tour_duration*num_ci;
 						}
-				}
+					}	
 					
-					bookableitems = bookableitems+(single_cost * num_item-discount);
+					bookableitems = bookableitems+((single_cost * num_item)-discount);
 				}
 				
 				
-				//total_cost for snowshoetour is cost of bookable item + guide cost -refundable amount?   //typecast
-				int totalcost = (int) ((bookableitems + tour_cost)*(participant.getRefundedPercentageAmount()*0.001));
-				//hold on how is totalcost an integer if there is a percentage refund? 
-				
+				//Calculate the total cost of the trip for a single participant 
+				int totalcost = (int) ((bookableitems + tour_cost)*(participant.getRefundedPercentageAmount()*0.001)); 
+				//Adding the info to the list of participants
 				participants.set(p, new TOParticipantCost(participant.getAccountName(),participant.getName(),bookableitems,totalcost));
 				
 			}
-			//how do you add participants to SnowShoeTour (setParticipants is private)
-			  
+			//Instancing TOSnowShoeTour
+			TOSnowShoeTour SnowShoeTour = new TOSnowShoeTour(id,tour.getStartWeek(),tour.getEndWeek(),tour.getGuide().getAccountName(),tour.getGuide().getName(),tour_cost,participants.toArray(new TOParticipantCost[0]));  
 			return SnowShoeTour;
 		}catch(Exception e) {
 			return null;
