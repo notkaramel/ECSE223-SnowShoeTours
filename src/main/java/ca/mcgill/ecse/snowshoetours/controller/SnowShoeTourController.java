d6305064d01edf8847e0c4d75c3a47146ce279b9
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2023-03-12 20:33:18 -0400

    Fixed getSnowShoeTour, need to add TOparticipant_Cost to TOSnowShoeTour

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourController.java
@@ -4,32 +4,79 @@ import java.sql.Date;
 
 import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
 
-import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
+import ca.mcgill.ecse.snowshoetours.model.*;
 
-//Check if necessary
-import ca.mcgill.ecse.snowshoetours.model.Tour;
+import java.util.*;
 
 public class SnowShoeTourController {
 	private static SnowShoeTour ssts = SnowShoeToursApplication.getSnowShoeTour();
 	
   public static TOSnowShoeTour getSnowShoeTour(int id) {
-    // TODO Implement the method 
-	if(!sst.hasTours()) {
+    // TODO Implement the method
+	//Can't find tour if there is no tour
+	if(!ssts.hasTours()) {
 		return null;
 	}
-	  id_dne =
-	
-	if (id_dne) {
-		return "Error: There is no tour with that id";
+	//If there is no tour with that id --> index out of range 
+	if (id <= 0 || id > ssts.numberOfTours()-1 ) {
+		return null;
 	}
+		
+	
 	else {
 		try {
-			Tour tour = sst.getTour(id);  
-			return tour;
+			Tour tour = ssts.getTour(id);
+			int tour_cost = ssts.getPriceOfGuidePerWeek()*(tour.getStartWeek()-tour.getEndWeek());
+			TOSnowShoeTour SnowShoeTour = new TOSnowShoeTour(id,tour.getStartWeek(),tour.getEndWeek(),tour.getGuide().getAccountName(),tour.getGuide().getName(),tour_cost);
+			//TOParticipant Attributes --> Create a TOParticipant object for each participant registered for the tour 
+			int p_no = tour.numberOfParticipants();
+			ArrayList <TOParticipantCost> participants = new ArrayList<TOParticipantCost>();
+			for(int p = 0; p < p_no ; p++) {
+				Participant participant = tour.getParticipant(p);
+				//Calculate the total cost for bookable items 
+				int bookableitems = 0;
+				for(int i = 0;i<participant.getBookedItems().size();i++) {
+					BookedItem booked_item = participant.getBookedItem(i);
+					BookableItem bookable_item = booked_item.getItem();
+					int num_item = booked_item.getQuantity();
+					int discount = 0; 
+					int single_cost = 0;
+					//Check if the item is a standalone gear or in a combo 
+					if (bookable_item instanceof Gear) {
+						Gear gear = (Gear)bookable_item;
+						single_cost = gear.getPricePerWeek() * ssts.getNrWeeks();
+						
+					}else if(bookable_item instanceof Combo) {
+						Combo combo = (Combo) bookable_item;
+						discount = combo.getDiscount();
+						//add the price of each type of item in the combo
+						for(int ci = 0; ci<combo.getBookedItems().size();ci ++) {
+							ComboItem combo_item= combo.getComboItem(ci);
+							int num_ci = combo_item.getQuantity();
+							single_cost = single_cost + combo_item.getGear().getPricePerWeek() * ssts.getNrWeeks()*num_ci;
+
+						}
+				}
+					
+					bookableitems = bookableitems+(single_cost * num_item-discount);
+				}
+				
+				
+				//total_cost for snowshoetour is cost of bookable item + guide cost -refundable amount?   //typecast
+				int totalcost = (int) ((bookableitems + tour_cost)*(participant.getRefundedPercentageAmount()*0.001));
+				//hold on how is totalcost an integer if there is a percentage refund? 
+				
+				participants.set(p, new TOParticipantCost(participant.getAccountName(),participant.getName(),bookableitems,totalcost));
+				
+			}
+			//how do you add participants to SnowShoeTour (setParticipants is private)
+			  
+			return SnowShoeTour;
 		}catch(Exception e) {
-			return "Error: Something went wrong";
+			return null;
 		}
 	}
+	  
   }
 
   public static String updateSnowShoeTour(Date startDate, int nrWeeks, int priceOfGuidePerWeek) {
