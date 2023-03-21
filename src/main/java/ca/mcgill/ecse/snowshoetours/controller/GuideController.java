4df42ba759182fe33ed4501fd7b6ab0fe3543598
SRIAZ77 <sameer.riaz@mail.mcgill.ca>
2023-03-21 14:46:56 -0400

    Fixed Guide Controller to pass all but 3 tests

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/GuideController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/GuideController.java
@@ -2,46 +2,64 @@ package ca.mcgill.ecse.snowshoetours.controller;
 
 import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
 import ca.mcgill.ecse.snowshoetours.model.Guide;
+import ca.mcgill.ecse.snowshoetours.model.Participant;
 import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
 import ca.mcgill.ecse.snowshoetours.model.User;
 
 public class GuideController {
 	private static SnowShoeTour sst = SnowShoeToursApplication.getSnowShoeTour();
-  public static String registerGuide(String email, String password, String name,
-      String emergencyContact) {
-    // TODO Implement the method, return error message (if any)
+  public static String registerGuide(String email, String password, String name, String emergencyContact) {
 	  
 	 // EMAIL VALIDATION
 	   // email must not contain any spaces
 	   if (email.contains(" ")) {
-		   return "Error: email can not contain spaces.";
+		   return "Email must not contain any spaces";
 	   }
+	   if (email == "" || email == null) {
+		return "Email cannot be empty";
+	}
 
-	   // email must contain some characters (anything is allowed except @), a @, some characters, a dot, and some characters
-	   // this is a simplified check sufficient for this application
+
+	//    email must contain some characters (anything is allowed except @), a @, some characters, a dot, and some characters
+	//    this is a simplified check sufficient for this application
 	   Boolean condition1 = email.indexOf("@") > 0; // index starts at zero
 	   Boolean condition2 = email.indexOf("@") == email.lastIndexOf("@");
 	   Boolean condition3 = email.indexOf("@") < email.lastIndexOf(".") - 1;
 	   Boolean condition4 = email.lastIndexOf(".") < email.length() - 1;
 	   
 	   if (!(condition1 && condition2 && condition3 && condition4)) {
-		   return "Error: Invalid email address";
+		   return "Invalid email";
 	   }
 	   if (name == "" || name == null) {
-		   return "Error: name can not be null";
+		   return "Name cannot be empty";
 	   }
 	   if (emergencyContact == "" || emergencyContact == null) {
-		   return "Error: emergency contact can not be null";
+		   return "Emergency contact cannot be empty";
 	   }
 	   if (password == "" || password == null) {
-		   return "Error: password can not be null";
+		   return "Password cannot be empty";
 	   }
-	   if (email == "manager" || Guide.hasWithAccountName(email)) {
-		   return "Error: This account name is not allowed";
+	   if (email == "manager") {
+		   return "Invalid email";
 	   }
+
+	   if (User.hasWithAccountName(email)){
+		if (User.getWithAccountName(email) instanceof Participant){
+			return "Email already linked to a participant account";
+		}
+		if (User.getWithAccountName(email) instanceof Guide){
+			return "Email already linked to a guide account";
+		}
+		else { // IF USER IS MANAGER
+			return "Invalid email";
+
+		}
+
+	   }
+	   
 	   else {
 		   try {
-			   sst.addGuide(name, password, name, emergencyContact);
+			   sst.addGuide(email, password, name, emergencyContact);
 			   return "";
 		   }
 		   catch (Exception e) {
@@ -53,7 +71,7 @@ public class GuideController {
 
   public static void deleteGuide(String email) {
 	  if (Guide.hasWithAccountName(email)) {
-		  try { User.getWithAccountName(email).delete();
+		  try { Guide.getWithAccountName(email).delete();
 	  }
 		  catch (Exception e) {
 		  }
