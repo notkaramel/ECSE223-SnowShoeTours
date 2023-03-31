bedc92434c7414428c707208887d2ef0cd3f3e1c
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2023-03-31 12:34:35 -0400

    Completed Given the following participants exist in the system

--- src/test/java/ca/mcgill/ecse/snowshoetours/features/ToursStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/snowshoetours/features/ToursStepDefinitions.java
@@ -76,15 +76,21 @@ public class ToursStepDefinitions {
 
     // Angela
     @Given("the following participants exist in the system")
-    public void the_following_participants_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void the_following_participants_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {  
+    	List<Map<String,String>> rows = dataTable.asMaps();
+    	for (var row:rows) {
+    		String email = String.valueOf(row.get("email"));
+    		String password = String.valueOf(row.get("password"));
+    		String name = String.valueOf(row.get("name"));
+    		String emergency_contact = String.valueOf(row.get("emergencyContact"));
+    		int nr_weeks=Integer.valueOf(row.get("nrWeeks"));
+    		int week_from = Integer.valueOf(row.get("weeksAvailableFrom"));
+    		int week_until = Integer.valueOf(row.get("weeksAvailableUntil"));
+    		boolean lodge_required = Boolean.valueOf(row.get("lodgeRequired"));
+    		sst.addParticipant(email, password, name, emergency_contact, nr_weeks, week_from, week_until, lodge_required,"",0);
+    		
+    	}
+        
     }
 
     // Angela
