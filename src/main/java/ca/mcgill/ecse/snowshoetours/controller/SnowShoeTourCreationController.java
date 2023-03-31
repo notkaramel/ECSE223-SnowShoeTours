843a818c872817d2f9032c02f6db19ba31dcafc9
Bilar <bilar.mokhtari@mail.mcgill.ca>
2023-03-31 15:23:31 -0400

    Added Persistence to my methods

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourCreationController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourCreationController.java
@@ -44,8 +44,11 @@ public class SnowShoeTourCreationController {
     } catch (Exception e) {
       return e.getMessage();
     }
-
+      
+    SnowShoeTourPersistence.save(); // Save the persistence after all tours have been assigned
+      
     return "";
+      
 }
 
 /**
@@ -56,25 +59,28 @@ public class SnowShoeTourCreationController {
    * @param authorizationCode
 	 * @return
 	 */
-public static String payForTrip(String email, String authorizationCode) {
-  Participant participant = getParticipantByEmail(email); 
+  public static String payForTrip(String email, String authorizationCode) {
+    Participant participant = getParticipantByEmail(email); // Get participant by email
 
-  if (participant == null) {
-      return String.format("Participant with email address %s does not exist", email);
-  }
+    if (participant == null) {
+        return String.format("Participant with email address %s does not exist", email); // Return error message if participant is null
+    }
 
-  if (authorizationCode == null || authorizationCode.isEmpty()) {
-      return "Invalid authorization code"; // Return an error message if the authorization code is missing or empty
-  }
+    if (authorizationCode == null || authorizationCode.isEmpty()) {
+        return "Invalid authorization code"; // Return error message if authorization code is missing or empty
+    }
 
-  try {
-      participant.pay();
-  } catch (Exception e) {
-      return e.getMessage();
-  }
+    try {
+        participant.pay(); // Process payment for participant
+        SnowShoeTourPersistence.save(); // Save persistence after payment is processed
+    } catch (Exception e) {
+        return e.getMessage(); // Return error message if payment processing or persistence saving fails
+    }
 
-  return "";
+    return ""; // Return empty string to indicate success
 }
+
+  
 
 /**
 	 * This method to start a trip for participants for during a specific week
@@ -83,28 +89,34 @@ public static String payForTrip(String email, String authorizationCode) {
    * @param week
 	 * @return
 	 */
-public static String startAllTripsForSpecificWeek(int week) {
-  List<Tour> shoeTours = sst.getTours(); // Get all tours
+  public static String startAllTripsForSpecificWeek(int week) {
+    List<Tour> shoeTours = sst.getTours(); // Get all tours in the system
+    boolean tripStarted = false; // Flag to keep track of whether any trips were started
 
-  boolean tripStarted = false; // Flag to keep track of whether any trips were started
-
-  for (Tour shoeTour : shoeTours) { // Loop through all tours
-      if (shoeTour.getStartWeek() == week) { // Check if tour is starting in the specified week
-          for (Participant participant : shoeTour.getParticipants()) { // Loop through all participants in the tour
-              if (!participant.hasTour()) { // Check if the participant has not already started the tour
-                  participant.startTour(); // Start the tour for the participant
-                  tripStarted = true; // Set flag to indicate that a trip was started
-              }
-          }
-      }
-  }
+    for (Tour shoeTour : shoeTours) { // Loop through all tours
+        if (shoeTour.getStartWeek() == week) { // Check if tour is starting in the specified week
+            for (Participant participant : shoeTour.getParticipants()) { // Loop through all participants in the tour
+                if (!participant.hasTour()) { // Check if the participant has not already started the tour
+                    participant.startTour(); // Start the tour for the participant
+                    tripStarted = true; // Set flag to indicate that a trip was started
+                }
+            }
+        }
+    }
 
     if (tripStarted) { // Check if any trips were started
-      return ""; // Return an empty string to indicate that the trips were started successfully
-  } else {
-      return String.format("No trips were started for %s", week) ; // Return a message indicating that no trips were started
+        try {
+            SnowShoeTourPersistence.save(); // Save the updated system state
+        } catch (Exception e) {
+            return e.getMessage(); // Return an error message if the system state could not be saved
+        }
+        return ""; // Return an empty string to indicate that the trips were started successfully
+    } else {
+        return String.format("No trips were started for week %s", week); // Return a message indicating that no trips were started
     }
 }
+
+
 
 /**
 	 * This method to finish a trip for a specific participant
@@ -114,21 +126,23 @@ public static String startAllTripsForSpecificWeek(int week) {
    * @param email : The AccountName of a participant
 	 * @return
 	 */
-public static String finishParticipantTrip(String email) {
-  Participant participant = getParticipantByEmail(email); // Get the participant by email [NOT IMPLEMENTED]
-
-  if (participant == null) {
-      return String.format("Participant with email address %s does not exist", email); // Use string interpolation for the error message 
+  public static String finishParticipantTrip(String email) {
+    Participant participant = getParticipantByEmail(email);
+  
+    if (participant == null) {
+        return String.format("Participant with email address %s does not exist", email);
+    }
+  
+    try {
+        participant.finish();
+        SnowShoeTourPersistence.save(); // Save persistence after the participant's tour is finished
+    } catch (RuntimeException e) {
+        return e.getMessage();
+    }
+  
+    return "";
   }
-
-  try {
-      participant.finish(); // Finish the participant's tour
-  } catch (RuntimeException e) {
-      return e.getMessage(); // Return the error message from the exception
-  }
-
-  return ""; // Return an empty string to indicate success
-}
+  
 
 /**
 	 * This method to cancel a trip for participants
@@ -138,20 +152,30 @@ public static String finishParticipantTrip(String email) {
    * @param week
 	 * @return
 	 */
-public static String cancelParticipantTrip(String email) {
-  Participant participant = getParticipantByEmail(email);
-
-  if (participant == null) {
-      return String.format("Participant with email address %s does not exist", email);
+  public static String cancelParticipantTrip(String email) {
+    // Get the participant by email
+    Participant participant = getParticipantByEmail(email);
+  
+    if (participant == null) {
+        // Use string interpolation for the error message
+        return String.format("Participant with email address %s does not exist", email);
+    }
+  
+    if (!participant.hasTour()) {
+        return "Participant has not started their tour";
+    }
+  
+    participant.cancel(); // Cancel the participant's tour
+  
+    try {
+        SnowShoeTourPersistence.save(); // Save persistence after the participant's tour is cancelled
+    } catch (Exception e) {
+        return e.getMessage(); // Return the error message from the exception
+    }
+  
+    return ""; // Return an empty string to indicate success
   }
-
-  if (!participant.hasTour()) {
-      return "Participant has not started their tour";
-  }
-
-  participant.cancel();
-  return "";
-}
+  
 
 
 
