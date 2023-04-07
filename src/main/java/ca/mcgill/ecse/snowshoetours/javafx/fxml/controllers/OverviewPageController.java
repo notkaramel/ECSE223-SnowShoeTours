91321ea356e52c7fc26ee4309a908cbce5c1cf3e
Antoine <antoine.phan@mail.mcgill.ca>
2023-04-07 16:31:15 -0400

    Skeleton for the Main Page & Overview

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/OverviewPageController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/OverviewPageController.java
@@ -1,8 +1,22 @@
 package ca.mcgill.ecse.snowshoetours.javafx.fxml.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 /**
  * @author Antoine Phan @notkaramel
- */
+ */ 
 public class OverviewPageController {
+    @FXML
+    private ResourceBundle resources;
     
-}
+    @FXML
+    private URL location;
+    
+    @FXML
+    void initialize() {
+        
+    }    
+    
+}    
