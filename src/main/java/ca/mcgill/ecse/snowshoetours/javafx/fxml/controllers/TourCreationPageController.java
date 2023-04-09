6c77005685013024ac8380b2f54afe25df10cc10
Bilar <bilar.mokhtari@mail.mcgill.ca>
2023-04-09 17:30:09 -0400

    Bruh

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/TourCreationPageController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/TourCreationPageController.java
@@ -1,8 +1,27 @@
 package ca.mcgill.ecse.snowshoetours.javafx.fxml.controllers;
 
-/**
- * @author Bilar Mokhtari @bmokhtari
- */
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import ca.mcgill.ecse.snowshoetours.controller.SnowShoeTourCreationController;
 public class TourCreationPageController {
-    
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initiateSnowToursCreation(MouseEvent event) {
+        SnowShoeTourCreationController.initiateSnowToursCreation();
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
+
